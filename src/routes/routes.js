dea2ab016c3b79a4c98235a4536aeeea7c15f1fb
Arepalli Praveenkumar <arepalli.praveen6192@gmail.com>
2020-03-24 20:19:03 +0530

    routes issue fix

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -20,7 +20,7 @@ class RoutesComponents extends React.Component {
     render() {
         return (
             
-            <Router basename={process.env.PUBLIC_URL}>
+            <Router basename='/'>
                 <nav className="navbar navbar-expand-sm bg-dark navbar-dark">
                 <ul className="navbar-nav">
                     <li className="nav-item active">
