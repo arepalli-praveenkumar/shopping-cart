f854d8603da98356299531aa56c620231cf5977e
Arepalli Praveenkumar <arepalli.praveen6192@gmail.com>
2020-04-09 19:29:20 +0530

    Updates

---
+++ precache-manifest.b9063e274f2ef189227683f674d8417c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5e32ecd70a7134c6e0f689280d98807",
+    "url": "/shopping-cart/index.html"
+  },
+  {
+    "revision": "33cfb22feb23086f386f",
+    "url": "/shopping-cart/static/css/main.16f9f40d.chunk.css"
+  },
+  {
+    "revision": "864ab81beefc4042b8ec",
+    "url": "/shopping-cart/static/js/2.83911725.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/shopping-cart/static/js/2.83911725.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33cfb22feb23086f386f",
+    "url": "/shopping-cart/static/js/main.79b23830.chunk.js"
+  },
+  {
+    "revision": "e2b9013e0cc846903c56",
+    "url": "/shopping-cart/static/js/runtime-main.bcdff60e.js"
+  },
+  {
+    "revision": "309e214843d010c7d09841bd12267908",
+    "url": "/shopping-cart/static/media/home-page-bg.309e2148.jpg"
+  },
+  {
+    "revision": "b55d9130fcc2620e1bd7ce952f7f0d7b",
+    "url": "/shopping-cart/static/media/shopping-cart.b55d9130.jfif"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/shopping-cart/precache-manifest.d9c1f7ecb0584cfdf9b5bb5206f2b45f.js"
+  "/shopping-cart/precache-manifest.b9063e274f2ef189227683f674d8417c.js"
 );
 
 self.addEventListener('message', (event) => {
