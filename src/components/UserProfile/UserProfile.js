9e48ac15e0834bb8493ec9e07ac81e85b5d42ce6
Arepalli Praveenkumar <arepalli.praveen6192@gmail.com>
2020-04-16 11:21:42 +0530

    profile pic

--- src/components/UserProfile/UserProfile.js
+++ src/components/UserProfile/UserProfile.js
@@ -16,7 +16,8 @@ class UserProfile extends React.Component {
       email : "",
       username : ""
       },
-      token : ""
+      token : "",
+      profileImage : ""
     }
   }
 
@@ -26,19 +27,26 @@ class UserProfile extends React.Component {
     console.log(fieldName, fieldValue);
     this.setState((state) => {
         state.userInfo[fieldName] = fieldValue;
+        console.log(state.userInfo)
     })
   }
 
   updateProfile = (event) => {
     let loggedUser = JSON.parse(sessionStorage.getItem("user"));
     let newProfile = {
-      name : "Arepalli Praveenkumar",
-      phoneNo : 784512369,
+      name : this.state.userInfo.name,
+      phoneNo : this.state.userInfo.phoneNo,
       userID : loggedUser.id
     };
     event.preventDefault();
     this.props.saveUserProfile(newProfile);
     console.log(this.state.userInfo, this.state.token);
+  }
+
+  loadImage = (event) => {
+    this.setState({
+      profileImage: URL.createObjectURL(event.target.files[0])
+    })
   }
 
 shouldComponentUpdate() {
@@ -60,22 +68,27 @@ render() {
      <div className="profile-from-wrap">
           <h1>Profile Information</h1>
           <form>
-          <div className="form-group">
-            <label>Full Name</label>
-            <input type="text" name="name" placeholder="full name"
-             onChange={this.inputHandler} value={name}/>
-          </div>
-          <div className="form-group">
-            <label>Phone Number</label>
-            <input type="text" name="phoneNo" placeholder="phone no"
-             className="form-control" onChange={this.inputHandler} value={phoneNo}/>
-          </div>
-          <button className="my-btn" onClick={this.updateProfile}>Update Profile</button>
-          
-
-        </form>
-
-        </div>
+            <div className="profile-pic-sec">
+              {/* <input type="file" onChange={this.loadImage}/> */}
+              <img  className="profile-pic" src={this.state.profileImage}/>
+              <input id="praveen" type="file" className="km-btn-file" onChange={this.loadImage} ></input>
+              <label htmlFor="praveen" className="km-button km-button--primary km-btn-file-label">
+                  <span>Upload</span>
+              </label>
+            </div>
+            <div className="form-group">
+              <label>Full Name</label>
+              <input type="text" defaultValue={name} name="name" placeholder="full name"
+              onChange={this.inputHandler}/>
+            </div>
+            <div className="form-group">
+              <label>Phone Number</label>
+              <input type="text" defaultValue={phoneNo} name="phoneNo" placeholder="phone no"
+              className="form-control" onChange={this.inputHandler} />
+            </div>
+            <button className="my-btn" onClick={this.updateProfile}>Update Profile</button>
+          </form>
+      </div>
    :  <div>...Loading</div>
 
   return (
